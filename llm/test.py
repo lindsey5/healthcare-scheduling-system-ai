7e1d07e82099296827aeeb915b11d017eebd9f9a
lindseysamson5 <lindseysamson5@gmail.com>
2026-08-08 05:07:04 +0800

    Refactor config and test files to remove unused GEMINI_API_KEY handling and update model import for consistency

--- llm/test.py
+++ llm/test.py
@@ -1,11 +1,9 @@
-import os
-from langchain_google_genai import GoogleGenerativeAI
 from dotenv import load_dotenv
 
-from llm.model import get_qwen3_4b_model
+from llm.model import get_healthcare_ai_model
 
 # Load environment variables from .env if running locally
 load_dotenv()
-llm = get_qwen3_4b_model()
+llm = get_healthcare_ai_model()
 
 print(llm.invoke("Hello"))
